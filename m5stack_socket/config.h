e94baece9884a03b35cde84990f9d3510784cf15
Shozaburo Shimada <shozaburo.shimada@spods.jp>
2025-09-19 16:58:20 +0900

    update

--- m5stack_socket/config.h
+++
@@ -1,7 +0,0 @@
-// WiFi configuration
-const char* ssid = "SSID";
-const char* pass = "PASS";
-
-//Server configuration
-const char* server_host = "HOST IP";
-const uint16_t server_port = PORT_NUMBER;
